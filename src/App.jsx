87a92af830034ae3db242f54fc2b683ee899573d
ogecc <ojerotijevic@gmail.com>
2026-06-23 14:22:15 +0200

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Checkout from './pages/Checkout'
+import Auth from './pages/Auth'
+import Navbar from './components/Navbar'
 
 function App() {
-
-
   return (
-    <>
-    </>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
   )
 }
 
