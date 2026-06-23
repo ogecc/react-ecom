cab1e725ce93fa45c6cd4f33d963c23d47f0ddfd
ogecc <ojerotijevic@gmail.com>
2026-06-23 18:29:43 +0200

    Auth added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import Home from './pages/Home'
 import Checkout from './pages/Checkout'
 import Auth from './pages/Auth'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 
 function App() {
   return (
     <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </AuthProvider>
+      
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
             <h2 className="page-title">Our Products</h2>
             <div className='product-grid'>
                 {products.map((product) => (
-                    <ProductCard product={product} key={product.key} />
+                    <ProductCard product={product} key={product.id} />
                 ))}
             </div>
         </div>
